0ee8ca2fa9572a6d855f5f84452b333b4449bc06
dle <david.le@aubay.com>
2020-05-14 08:44:28 +0200

    ticket model correction

--- app/models/ticketingModel.js
+++ app/models/ticketingModel.js
@@ -3,25 +3,30 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const TicketingSchema = new Schema({
-  ticket_type: {
-    type: String
-  },
-  user: {
+  event: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: 'Event'
   },
-  last_name: {
-    type: String
-  },
-  first_name: {
-    type: String
-  },
-  adress: {
-    type: String
-  },
-  date_of_purchase: {
-    type: Date
-  }
+  ticket_type: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      last_name: {
+        type: String
+      },
+      first_name: {
+        type: String
+      },
+      adress: {
+        type: String
+      },
+      date_of_purchase: {
+        type: Date
+      }
+    }
+  ]
 })
 
 module.exports = TicketingSchema
